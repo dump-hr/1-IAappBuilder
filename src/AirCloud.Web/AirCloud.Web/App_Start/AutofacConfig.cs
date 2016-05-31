f43d7d29f28b82808174c2aa1d3b6b7517ff0ab4
Gabrijel Boduljak <Gabrijel Boduljak@DESKTOP>
2016-05-31 11:01:49 +0200

    set up web project, configured web api, configured swagger and autofac di, currenty assembly scanning doesnt work

---
+++ src/AirCloud.Web/AirCloud.Web/App_Start/AutofacConfig.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Http;
+using Autofac;
+using Autofac.Integration.WebApi;
+using Owin;
+
+namespace AirCloud.Web
+{
+    public static class AutofacConfig
+    {
+        public static void UseAutofacForWebApi(this IAppBuilder app, HttpConfiguration httpConfiguration)
+        {
+            var builder = new ContainerBuilder();
+            ConfigureAssemblyScanning(builder);
+            ConfigureCustomDependecyMappings(builder);
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+            builder.RegisterWebApiFilterProvider(httpConfiguration);
+            var container = builder.Build();
+
+            app.UseAutofacMiddleware(container);
+            app.UseAutofacWebApi(configuration: httpConfiguration);
+            httpConfiguration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        private static void ConfigureAssemblyScanning(ContainerBuilder builder)
+        {
+            var currentDomainAssemblies = AppDomain.CurrentDomain.GetAssemblies();
+
+            builder.RegisterAssemblyTypes(currentDomainAssemblies)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(currentDomainAssemblies)
+                .Where(t => t.Name.EndsWith("Service"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(currentDomainAssemblies)
+                .Where(t => t.Name.EndsWith("Command"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+
+            builder.RegisterAssemblyTypes(currentDomainAssemblies)
+                .Where(t => t.Name.EndsWith("Factory"))
+                .AsImplementedInterfaces()
+                .InstancePerRequest();
+        }
+
+        private static void ConfigureCustomDependecyMappings(ContainerBuilder builder)
+        {
+         
+        }
+    }
+}
---
+++ src/AirCloud.Web/AirCloud.Web/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AirCloud.Web.Startup))]
+
+namespace AirCloud.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseAutofacForWebApi(httpConfiguration: GlobalConfiguration.Configuration);      
+        }
+    }
+}
