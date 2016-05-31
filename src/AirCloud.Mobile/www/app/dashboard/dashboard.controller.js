8a187748a3410d39f8d323ccf12f3800cca5be60
Miroslav Jelaska <miro@dump.hr>
2016-05-31 11:06:13 +0200

    Air quality msgs

--- src/AirCloud.Mobile/www/app/dashboard/dashboard.controller.js
+++ src/AirCloud.Mobile/www/app/dashboard/dashboard.controller.js
@@ -1,14 +1,16 @@
 (function(){
     angular.module('app').controller('DashboardController', DashboardController);
 
-	DashboardController.$inject = ['$scope', '$ionicPlatform', '$rootScope']; 
-    function DashboardController($scope, $ionicPlatform, $rootScope) {
+	DashboardController.$inject = ['$scope', '$ionicPlatform', '$rootScope', 'airQualityStatusService']; 
+    function DashboardController($scope, $ionicPlatform, $rootScope, airQualityStatusService) {
         var vm = this;
         
         vm.initialDataLoaded = false;
         
+        
         $rootScope.$on('deviceDataEmitter:update', function (event, data) {
             vm.readings = data;
+            vm.quality = airQualityStatusService.getStatus(data);
             vm.initialDataLoaded = true;
         });
         $scope.$on("$ionicView.enter", function () {          
