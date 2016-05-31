254333cdd687f496eb3ae6d56099d4893f5043f6
Gabrijel Boduljak <Gabrijel Boduljak@DESKTOP>
2016-05-31 12:58:51 +0200

    redefined routes

--- src/AirCloud.Web/AirCloud.Web/App_Start/RouteConfig.cs
+++ src/AirCloud.Web/AirCloud.Web/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace AirCloud.Web
         
             routes.MapHttpRoute(
                name: "DefaultApi",
-               routeTemplate: "api/{controller}/{id}",
+               routeTemplate: "api/{controller}/{action}/{id}",
                defaults: new { id = RouteParameter.Optional }
             );
 
