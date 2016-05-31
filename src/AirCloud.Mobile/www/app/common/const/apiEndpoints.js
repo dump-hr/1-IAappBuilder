90701428a128b5c343e6f93f9a800a31c1c6fd87
Ivan Medić <medic.ivan.hr@gmail.com>
2016-05-31 23:43:03 +0200

    Minor mobile map improvments

--- src/AirCloud.Mobile/www/app/common/const/apiEndpoints.js
+++ src/AirCloud.Mobile/www/app/common/const/apiEndpoints.js
@@ -2,8 +2,8 @@
 
     angular.module('app').constant('apiEndpoints', {
         reading: {
-            getAll_LongDetails: 'http://localhost:50942/api/readings/getAll_longdetails',
-            create:             'http://localhost:50942/api/readings/create'
+            getAll_LongDetails: 'http://aircloud.dump.hr/api/readings/getAll_longdetails',
+            create:             'http://aircloud.dump.hr/api/readings/create'
         }
     });
 })();
