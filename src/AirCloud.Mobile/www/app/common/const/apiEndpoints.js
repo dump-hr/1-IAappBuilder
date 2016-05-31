b650c1361f5bc79a5c5e2350087b2dab684cd27e
Gabrijel Boduljak <Gabrijel Boduljak@DESKTOP>
2016-05-31 22:13:38 +0200

    implemented heatmap on mobile, works, parses both voc and co toweighted data points for google maps

---
+++ src/AirCloud.Mobile/www/app/common/const/apiEndpoints.js
@@ -0,0 +1,9 @@
+﻿(function () {
+
+    angular.module('app').constant('apiEndpoints', {
+        reading: {
+            getAll_LongDetails: 'http://aircloud.dump.hr/api/readings/getAll_longdetails',
+            create:             'http://aircloud.dump.hr/api/readings/create'
+        }
+    });
+})();
