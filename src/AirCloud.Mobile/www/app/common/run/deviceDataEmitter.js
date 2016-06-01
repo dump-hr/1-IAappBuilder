a61655d81168d1432ca0a86c8c40dd3528382861
cepki <ceepki@gmail.com>
2016-06-01 06:39:12 +0200

    fixed to 2

--- src/AirCloud.Mobile/www/app/common/run/deviceDataEmitter.js
+++ src/AirCloud.Mobile/www/app/common/run/deviceDataEmitter.js
@@ -26,7 +26,7 @@
                     return Math.random() * (max - min) + min;
                 }
 
-                var interval = 30000;
+                var interval = 1000;
                 function action() {
                     var newDataReading = {
                         voc: getNextRandomPercentage(),
