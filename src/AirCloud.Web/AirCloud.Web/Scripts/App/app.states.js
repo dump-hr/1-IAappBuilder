31018c1012d365f68d8a609e4798c16d838b9ce4
Ivan Medić <medic.ivan.hr@gmail.com>
2016-06-01 07:20:59 +0200

    old styles

--- src/AirCloud.Web/AirCloud.Web/Scripts/App/app.states.js
+++ src/AirCloud.Web/AirCloud.Web/Scripts/App/app.states.js
@@ -7,9 +7,12 @@
     function AppStates($stateProvider) {
         $stateProvider.state('Home', {
             url: '/',
+<<<<<<< HEAD
             templateUrl: './Scripts/App/landing/landing.template.html'
         }).state('Map', {
             url: '/map',
+=======
+>>>>>>> parent of 248f6a5... landing page header
             templateUrl: './Scripts/App/home/home.template.html',
             controller: 'homeController',
             controllerAs: 'vm'
