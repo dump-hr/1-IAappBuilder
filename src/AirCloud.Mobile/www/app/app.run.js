a159f6a79b9472d0cd7b38d6557a8150bfe6a9cc
Boris Borović <Boris Borovi>
2016-06-01 04:10:26 +0200

    Something

--- src/AirCloud.Mobile/www/app/app.run.js
+++ src/AirCloud.Mobile/www/app/app.run.js
@@ -3,8 +3,8 @@
 
 	angular.module('app').run(appConfig);
 
-	appConfig.$inject = ['$ionicPlatform', '$rootScope'];
-	function appConfig($ionicPlatform, $rootScope) {
+	appConfig.$inject = ['$ionicPlatform', '$rootScope', 'env'];
+	function appConfig($ionicPlatform, $rootScope, env) {
 		$ionicPlatform.ready(function ($ionicPlatform) {
 			if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
@@ -17,11 +17,12 @@
 		});
 
 		$rootScope.openIntercomMessagingCenter = function () {
-			$ionicPlatform.ready(function () {
-			
-				intercom.setPreviewPosition(intercom.BOTTOM_RIGHT);
-				intercom.displayMessageComposer();
-			});
+			if(env.isOnDevice) {
+				$ionicPlatform.ready(function () {
+					intercom.setPreviewPosition(intercom.BOTTOM_RIGHT);
+					intercom.displayMessageComposer();
+				});
+			}
 		}
 	}
 })(); 
