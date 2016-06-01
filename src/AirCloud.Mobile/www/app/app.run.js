b0caa2bc52c54483b54122e83fa63496b5cc7bcb
Miroslav Jelaska <miro@dump.hr>
2016-06-01 03:06:05 +0200

    added and configured intercom

--- src/AirCloud.Mobile/www/app/app.run.js
+++ src/AirCloud.Mobile/www/app/app.run.js
@@ -1,19 +1,27 @@
-(function() {
-	'use strict'; 
-	
+(function () {
+	'use strict';
+
 	angular.module('app').run(appConfig);
-	
-	appConfig.$inject = ['$ionicPlatform'];
-	function appConfig($ionicPlatform) {
-		$ionicPlatform.ready(function($ionicPlatform) {
+
+	appConfig.$inject = ['$ionicPlatform', '$rootScope'];
+	function appConfig($ionicPlatform, $rootScope) {
+		$ionicPlatform.ready(function ($ionicPlatform) {
 			if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
 				cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 				cordova.plugins.Keyboard.disableScroll(true);
-
 			}
 			if (window.StatusBar) {
 				StatusBar.styleDefault();
 			}
+			intercom.registerUnidentifiedUser();	
 		});
+
+		$rootScope.openIntercomMessagingCenter = function () {
+			$ionicPlatform.ready(function () {
+			
+				intercom.setPreviewPosition(intercom.BOTTOM_RIGHT);
+				intercom.displayMessageComposer();
+			});
+		}
 	}
 })(); 
